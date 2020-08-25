35b5a8f8e425c7e8e7ae38eb17f4b21848532264
kirankok <kirankokumar@gmail.com>
2020-08-25 14:26:29 +0530

    Updates

---
+++ precache-manifest.70496024f93846c3b235e40b25b1ac9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5a45ff3eb1397dbce8f657b3838c46e",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "be0708a5ab9509d3423f",
+    "url": "/netflix-clone/static/css/main.c202f337.chunk.css"
+  },
+  {
+    "revision": "983be4b97d1041a2696d",
+    "url": "/netflix-clone/static/js/2.fa081b74.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-clone/static/js/2.fa081b74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be0708a5ab9509d3423f",
+    "url": "/netflix-clone/static/js/main.0c38e015.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  }
+]);
